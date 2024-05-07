8a527e96c42c4c7f25d5f3fdf2032ebb50fb139b
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2024-05-07 08:33:28 +0200

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b bordder-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+        
+    </nav>
+}
+
+export default Navbar;
